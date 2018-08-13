9601c417cf662c119f9c6ef4af4f21413e92cd2f
Lan Tian <xuyh0120@outlook.com>
2018-08-14 00:12:12 +0800

    Add route-next image

---
+++ route-next/mk-compose.py
@@ -0,0 +1,64 @@
+import argparse
+class MakeComposeYml:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("first", help = "First IP of the network")
+        parser.add_argument("target", help = "Target IP of the network")
+        self.args = parser.parse_args()
+        ips = self.calculateNetwork(self.args.first, self.args.target)
+        self.f = open('docker-compose.yml', 'w')
+        self.writeHeader()
+        for i, ip in enumerate(ips):
+            self.writeContainer(i + 1, ip)
+        self.writeFooter()
+        self.f.close()
+    
+    def calculateNetwork(self, first, target):
+        first_split = first.split('.')
+        target_split = target.split('.')
+        if first_split[0:3] != target_split[0:3]:
+            raise ValueError("First and target not in same subnet")
+        if first_split[3] >= target_split[3]:
+            raise ValueError("First IP comes after target IP")
+        ips = []
+        new_ip = first_split
+        for last_space in range(int(first_split[3]), int(target_split[3]) + 1):
+            new_ip[3] = str(last_space)
+            ips.append('.'.join(new_ip))
+        return ips
+    
+    def writeHeader(self):
+        header = 'version: "2.1"\nservices:'
+        self.f.write(header)
+
+    def writeContainer(self, i, ip):
+        body = '''
+  routing%i:
+    image: xddxdd/route-img:latest
+    container_name: routing%i
+    restart: always
+    cap_add:
+      - NET_ADMIN
+    environment:
+      - TARGET_IP=%s
+    networks:
+      default:
+        ipv4_address: %s
+'''
+        self.f.write(body % (i, i, self.args.target, ip))
+    
+    def writeFooter(self):
+        footer = '''
+networks:
+  default:
+    driver: bridge
+    ipam:
+      driver: default
+      config:
+      - subnet: %s.0/24
+        gateway: %s.1
+'''
+        subnet = '.'.join(self.args.first.split('.')[0:3])
+        self.f.write(footer % (subnet, subnet))
+
+make = MakeComposeYml()
---
+++ route-next/route.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo Target IP is $TARGET_IP
+THIS_IP=$(ip addr show dev eth0 | grep inet | cut -d' ' -f6 | cut -d'/' -f1)
+echo My IP is $THIS_IP
+NEXT_IP=$(echo $THIS_IP | awk -F. '{print $1 "." $2 "." $3 "." $4 + 1}')
+if [ $THIS_IP == $NEXT_IP ]; then
+    echo I\'m the target, listening
+else
+    echo Routing $TARGET_IP to $NEXT_IP
+    ip route add $TARGET_IP/32 via $NEXT_IP
+fi
+ping 127.0.0.1 -q
