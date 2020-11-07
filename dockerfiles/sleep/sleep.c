a75bc6dc2d73a1f6a5f7c6d5b8426240fe49f8e1
Lan Tian <xuyh0120@outlook.com>
2020-11-07 18:16:04 +0800

    sleep: use sys_pause when possible

--- dockerfiles/sleep/sleep.c
+++ dockerfiles/sleep/sleep.c
@@ -2,5 +2,11 @@
 #include "syscall_arch.h"
 
 void _start() {
-	while(1) __syscall0(SYS_sched_yield);
+    while(1) {
+#ifdef SYS_pause
+        __syscall0(SYS_pause);
+#else
+        __syscall0(SYS_sched_yield);
+#endif
+    }
 }
